fe5422ae3b391e148eeb6dababdccdd85df3179c
giorgia-mineo <giomineo@gmail.com>
2023-05-09 15:05:21 +0100

    Added basic styles + content to Projects section

--- src/components/projects/leftAlign.tsx
+++ src/components/projects/leftAlign.tsx
@@ -1,6 +1,24 @@
-const LeftAlign = () => {
+import { NextPage } from "next"
+import Image from "next/image"
+
+interface Props {
+  image: string
+  title: string
+  tech: string
+  description: string
+}
+
+const LeftAlign: NextPage<Props> = (props) => {
+  const { title, image, tech, description } = props
   return (
-    <div>left</div>
+    <div className="container left-container">
+      <div className="photo" style={{ backgroundImage: `url('${image}')` }}></div>
+      <div className="text">
+        <h3>{title}</h3>
+        <p>{tech}</p>
+        <p>{description}</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/projects/rightAlign.tsx
+++ src/components/projects/rightAlign.tsx
@@ -1,6 +1,23 @@
-const RightAlign = () => {
+import { NextPage } from "next"
+
+interface Props {
+  image: string
+  title: string
+  tech: string
+  description: string
+}
+
+const RightAlign: NextPage<Props> = (props) => {
+  const { image, title, tech, description } = props
   return (
-    <div>right</div>
+    <div className="container right-container">
+      <div className="text">
+        <h3>{title}</h3>
+        <p>{tech}</p>
+        <p>{description}</p>
+      </div>
+      <div className="photo" style={{ backgroundImage: `url('${image}')` }}></div>
+    </div>
   )
 }
 
