9550d150137b112c470f57117802860ac39febc1
giorgia-mineo <giomineo@gmail.com>
2023-05-09 12:24:12 +0100

    Added basic styles for Contact section + added contact icons + fixed positioning and sizing throughout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,19 @@
+import '../app/styles/contact.scss'
+import Image from 'next/image'
+
 const Contact = () => {
   return (
     <section id="contact">
-      <div className="container">
-        <div>Contact me</div>
+      <div className="container contact-container">
+        <h2>Contact me</h2>
         <p>I&apos;m always happy to receive mail! Whether you have a question or just want to say hi, I&apos;d love to hear from you.</p>
-        LinkedIn | GitHub | Gmail
+
+        <div>
+          <a target="_blank" href="https://www.linkedin.com/in/giorgia-rm/"><Image alt={'linkedin'} src={'/icons/linkedin.png'} height={30} width={30} /></a>
+          <a target="_blank" href="https://github.com/giomine"><Image alt={'github'} src={'/icons/github-square.png'} height={30} width={30} /></a>
+          <a target="_blank" href="mailto:giomineo@gmail.com"><Image alt={'gmail'} src={'/icons/gmail.png'} height={30} width={40} /></a>
+        </div>
+        
       </div>
     </section>
   )
