998e4a850d36275358ffaef324533344fc514c0e
giorgia-mineo <giomineo@gmail.com>
2023-05-10 17:14:55 +0100

    Created custom navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,52 @@
+'use client'
+import { useState } from 'react'
+import '../app/styles/navbar.scss'
 import Link from "next/link"
 
 export default function Navbar(){
 
+  const [ dropdown, setDropdown ] = useState(true)
+
+  const handleClick = () => {
+    setDropdown(!dropdown)
+    console.log(dropdown)
+  }
+
   return (
-    <div className="navbar">
-      <Link href={'#home'}>Home</Link>
-      <Link href={'#about'}>About</Link>
-      <Link href={'#experience'}>Experience</Link>
-      <Link href={'#skills'}>Skills</Link>
-      <Link href={'#projects'}>Projects</Link>
-      <Link href={'#work'}>Work</Link>
-      <Link href={'#hobbies'}>Hobbies</Link>
-      <Link href={'#contact'}>Contact</Link>
-    </div>
+    <>
+      <div className="navbar-sm">
+        <div className='burger' onClick={handleClick}>Burger</div>
+        { dropdown === true ? 
+        (
+        <div className='nav-links'>
+          <Link onClick={handleClick} href={'#about'}>About</Link>
+          <Link onClick={handleClick} href={'#experience'}>Experience</Link>
+          <Link onClick={handleClick} href={'#skills'}>Skills</Link>
+          <Link onClick={handleClick} href={'#projects'}>Projects</Link>
+          <Link onClick={handleClick} href={'#work'}>Work</Link>
+          <Link onClick={handleClick} href={'#hobbies'}>Hobbies</Link>
+          <Link onClick={handleClick} href={'#contact'}>Contact</Link>
+        </div>
+        )
+        : '' }
+      </div>
+
+
+      <div className="navbar-lg">
+        <div className='container'>
+          <div>Logo</div>
+          <div className='link-container'>
+            <Link href={'#about'}>About</Link>
+            <Link href={'#experience'}>Experience</Link>
+            <Link href={'#skills'}>Skills</Link>
+            <Link href={'#projects'}>Projects</Link>
+            <Link href={'#work'}>Work</Link>
+            <Link href={'#hobbies'}>Hobbies</Link>
+            <Link href={'#contact'}>Contact</Link>
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
