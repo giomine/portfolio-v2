c3165b9b27a2be12d32dfbf800aef006eeccab97
giorgia-mineo <giomineo@gmail.com>
2023-05-11 16:55:04 +0100

    Added Hobbies icons + fixed margins to match

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -7,17 +7,20 @@ const Hobbies = () => {
         <h2>Hobbies</h2>
         
         <div className='grid'>
-          <div>
+          <div className='grid-div'>
+            <div className='hobby-logo cycle'></div>
             <h3>Cycling</h3>
             <p>I love exploring nature and cityscapes, and I collect photos of beautiful scenery - especially in Lincolnshire!</p>
           </div>
 
-          <div>
+          <div className='grid-div'>
+            <div className='hobby-logo langs'></div>
             <h3>Languages</h3>
             <p>I really enjoy decoding foreign languages and love how this opens me up to so much culture. Each country has its own special feel, and being able to immerse myself in different experiences is really special.</p>
           </div>
 
-          <div>
+          <div className='grid-div'>
+            <div className='hobby-logo gaming'></div>
             <h3>Gaming</h3>
             <p>Games are a passion for a similar reason as the above - they are an amazing way to have new experiences! As a player you get to explore so many imaginative worlds and stories along with beautiful artwork and music.</p>
           </div>
