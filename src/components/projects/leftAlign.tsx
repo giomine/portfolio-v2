4eccf5f763302b9c5741439179eea44f9b03eb01
giorgia-mineo <giomineo@gmail.com>
2023-05-11 22:17:26 +0100

    Changed Project imgs to Image tags which improved responsiveness + then could remove all button margin media queries

--- src/components/projects/leftAlign.tsx
+++ src/components/projects/leftAlign.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next"
 import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
   image: string
@@ -15,7 +16,7 @@ const LeftAlign: NextPage<Props> = (props) => {
   return (
     <div className="container left-container">
         <div className="photo-buttons">
-          <Link className="photo" style={{ backgroundImage: `url('${image}')` }} target="_blank" href={sitelink}></Link>
+          <Link className="photos" target="_blank" href={sitelink}><Image alt="project" src={image} width={600} height={320}></Image></Link>
           <div className="buttons">
             <Link target="_blank" href={codelink}><button><div className="logo"></div>ReadMe</button></Link>
             <Link target="_blank" href={sitelink}><button><div className="logo leave"></div>Site</button></Link>
--- src/components/projects/rightAlign.tsx
+++ src/components/projects/rightAlign.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next"
 import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
   image: string
@@ -21,7 +22,7 @@ const RightAlign: NextPage<Props> = (props) => {
       </div>
 
       <div className="photo-buttons">
-        <Link className="photo" style={{ backgroundImage: `url('${image}')` }} target="_blank" href={sitelink}></Link>
+        <Link className="photos" target="_blank" href={sitelink}><Image alt="project" src={image} width={600} height={320}></Image></Link>
         <div className="buttons">
           <Link target="_blank" href={codelink}><button><div className="logo"></div>ReadMe</button></Link>
           <Link target="_blank" href={sitelink}><button><div className="logo leave"></div>Site</button></Link>
